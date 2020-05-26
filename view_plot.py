60f59b95462f86c6ef7026d482ad55868f149e76
Donkere-vader <ctweeduizendvier@yahoo.com>
2020-05-26 11:23:14 +0200

    Updated view plot to export and view better

--- view_plot.py
+++ view_plot.py
@@ -1,11 +1,23 @@
 import matplotlib.pyplot as plt
 import json
+import numpy as np
+import datetime
 
 data = json.load(open('export.json'))
-plt.plot(data['infected'])
-plt.plot(data['dead'])
 
-plt.ylabel('Persons')
-plt.xlabel("Simulation ticks")
+x = [i for i in range(len(data['untouched']))]
+y1 = data['dead']
+y2 = data['infected']
+y3 = data['recovered']
+y4 = data['untouched']
 
+y = np.vstack([y1, y2, y3])
+
+labels = ["Dead ", "Infected", "Recovered", "Infectable"]
+
+fig, ax = plt.subplots()
+ax.stackplot(x, y1, y2, y3, y4, labels=labels)
+ax.legend(loc='upper left')
+date = datetime.datetime.now()
+plt.savefig(f"Virus plot export {date.day}-{date.month}-{date.year} {date.hour}_{date.minute}.{date.second}.png")
 plt.show()
